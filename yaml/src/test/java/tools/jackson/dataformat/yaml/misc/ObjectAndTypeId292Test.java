344f2e32023ac624dd9c60baa3f0ccc180e0a950
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-04-02 19:51:21 -0700

    Refactor #292 test

--- yaml/src/test/java/tools/jackson/dataformat/yaml/misc/ObjectAndTypeId292Test.java
+++ yaml/src/test/java/tools/jackson/dataformat/yaml/misc/ObjectAndTypeId292Test.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 /**
  * Passing tests related to [dataformats-text#292]: YAML anchor/alias
  * with type info and object identity. These variants work; the failing
- * variant (interface + builder) is in {@code tofix/ObjectAndTypeId292Test}.
+ * variants (with builder) are in {@code tofix/ObjectAndTypeId292Test}.
  */
 public class ObjectAndTypeId292Test extends ModuleTestBase
 {
@@ -52,44 +52,6 @@ public class ObjectAndTypeId292Test extends ModuleTestBase
     static class DerivedFromClass extends BaseClass {
         @JsonProperty
         public String a;
-    }
-
-    // Class base type + builder
-    static class ContainerClassWithBuilder {
-        @JsonProperty
-        public List<BaseClassWithBuilder> list;
-    }
-
-    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
-    @JsonSubTypes({@JsonSubTypes.Type(name = "DerivedClassBuilder", value = DerivedClassWithBuilder.class)})
-    @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class,
-            resolver = SimpleObjectIdResolver.class)
-    static class BaseClassWithBuilder {
-    }
-
-    @tools.jackson.databind.annotation.JsonDeserialize(builder = DerivedClassWithBuilder.DerivedBuilder.class)
-    static class DerivedClassWithBuilder extends BaseClassWithBuilder {
-        @JsonProperty
-        public String a;
-
-        DerivedClassWithBuilder(String a) {
-            this.a = a;
-        }
-
-        @tools.jackson.databind.annotation.JsonPOJOBuilder(withPrefix = "", buildMethodName = "build")
-        public static class DerivedBuilder {
-            private String a;
-
-            @JsonProperty
-            public DerivedBuilder a(String a) {
-                this.a = a;
-                return this;
-            }
-
-            public DerivedClassWithBuilder build() {
-                return new DerivedClassWithBuilder(this.a);
-            }
-        }
     }
 
     private final ObjectMapper MAPPER = newObjectMapper();
@@ -136,24 +98,4 @@ public class ObjectAndTypeId292Test extends ModuleTestBase
         assertSame(first, second);
     }
 
-    // [dataformats-text#292]: alias with class base type + builder works
-    @Test
-    public void testAliasWithClassAndBuilder() throws Exception
-    {
-        String yaml = "list:\n"
-                + "    - !DerivedClassBuilder &id1\n"
-                + "        a: foo\n"
-                + "    - *id1\n";
-        ContainerClassWithBuilder container = MAPPER.readValue(yaml, ContainerClassWithBuilder.class);
-        assertNotNull(container);
-        assertNotNull(container.list);
-        assertEquals(2, container.list.size());
-
-        BaseClassWithBuilder first = container.list.get(0);
-        assertEquals(DerivedClassWithBuilder.class, first.getClass());
-        assertEquals("foo", ((DerivedClassWithBuilder) first).a);
-
-        BaseClassWithBuilder second = container.list.get(1);
-        assertSame(first, second);
-    }
 }
--- yaml/src/test/java/tools/jackson/dataformat/yaml/tofix/ObjectAndTypeId292Test.java
+++ yaml/src/test/java/tools/jackson/dataformat/yaml/tofix/ObjectAndTypeId292Test.java
@@ -15,15 +15,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 /**
- * Test for [dataformats-text#292]: YAML anchor/alias with interface
+ * Test for [dataformats-text#292]: YAML anchor/alias with
  * base type + builder causes UnresolvedForwardReference.
  *<p>
  * NOTE: this is actually a jackson-databind issue (same failure with JSON),
  * not YAML-specific. Object identity does not work when combining
- * interface base type + {@code @JsonDeserialize(builder=...)} + polymorphism.
+ * base type + {@code @JsonDeserialize(builder=...)} + polymorphism.
  */
 public class ObjectAndTypeId292Test extends ModuleTestBase
 {
+    // Interface base type + builder
     static class ContainerWithBuilder {
         @JsonProperty
         public List<BaseWithBuilder> list;
@@ -61,9 +62,47 @@ public class ObjectAndTypeId292Test extends ModuleTestBase
         }
     }
 
+    // Class base type + builder
+    static class ContainerClassWithBuilder {
+        @JsonProperty
+        public List<BaseClassWithBuilder> list;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+    @JsonSubTypes({@JsonSubTypes.Type(name = "DerivedClassBuilder", value = DerivedClassWithBuilder.class)})
+    @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class,
+            resolver = SimpleObjectIdResolver.class)
+    static class BaseClassWithBuilder {
+    }
+
+    @tools.jackson.databind.annotation.JsonDeserialize(builder = DerivedClassWithBuilder.DerivedBuilder.class)
+    static class DerivedClassWithBuilder extends BaseClassWithBuilder {
+        @JsonProperty
+        public String a;
+
+        DerivedClassWithBuilder(String a) {
+            this.a = a;
+        }
+
+        @tools.jackson.databind.annotation.JsonPOJOBuilder(withPrefix = "", buildMethodName = "build")
+        public static class DerivedBuilder {
+            private String a;
+
+            @JsonProperty
+            public DerivedBuilder a(String a) {
+                this.a = a;
+                return this;
+            }
+
+            public DerivedClassWithBuilder build() {
+                return new DerivedClassWithBuilder(this.a);
+            }
+        }
+    }
+
     private final ObjectMapper MAPPER = newObjectMapper();
 
-    // [dataformats-text#292]
+    // [dataformats-text#292]: interface base type + builder
     @JacksonTestFailureExpected
     @Test
     public void testAliasWithInterfaceAndBuilder() throws Exception
@@ -82,6 +121,28 @@ public class ObjectAndTypeId292Test extends ModuleTestBase
         assertEquals("foo", ((DerivedWithBuilder) first).a);
 
         BaseWithBuilder second = container.list.get(1);
+        assertSame(first, second);
+    }
+
+    // [dataformats-text#292]: class base type + builder
+    @JacksonTestFailureExpected
+    @Test
+    public void testAliasWithClassAndBuilder() throws Exception
+    {
+        String yaml = "list:\n"
+                + "    - !DerivedClassBuilder &id1\n"
+                + "        a: foo\n"
+                + "    - *id1\n";
+        ContainerClassWithBuilder container = MAPPER.readValue(yaml, ContainerClassWithBuilder.class);
+        assertNotNull(container);
+        assertNotNull(container.list);
+        assertEquals(2, container.list.size());
+
+        BaseClassWithBuilder first = container.list.get(0);
+        assertEquals(DerivedClassWithBuilder.class, first.getClass());
+        assertEquals("foo", ((DerivedClassWithBuilder) first).a);
+
+        BaseClassWithBuilder second = container.list.get(1);
         assertSame(first, second);
     }
 }
